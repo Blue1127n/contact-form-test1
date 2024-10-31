6e0174f3f0219f5f9fcad0d74dd75a6eef74ef9e
ao1127 <yohijet271@gmail.com>
2024-10-31 14:29:31 +0900

    入力確認ページの変更

---
+++ src/app/Http/Controllers/ContactController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+
+    public function index()
+{
+    return view('index');
+}
+
+    public function confirm(Request $request)
+{
+    $contact = $request->only(['first-name','last-name','gender', 'email', 'tel','address','building','select', 'content']);
+    return view('confirm', compact('contact'));
+}
+
+public function store()
+{
+$contact = $request->only(['first-name','last-name','gender', 'email', 'tel','address','building','select', 'content']);
+}
+
+}
